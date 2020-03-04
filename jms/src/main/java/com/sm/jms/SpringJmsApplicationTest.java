5567f88ab4f0e9999205f43e9314f27e310f472c
Saurabh <mail2saurabhm@gmail.com>
2020-03-03 22:32:03 -0500

    JMS: Spring JMS Template with ActiveMQ

---
+++ jms/src/main/java/com/sm/jms/SpringJmsApplicationTest.java
@@ -0,0 +1,28 @@
+package com.sm.jms;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJmsApplicationTest {
+
+  @Autowired
+  private Sender sender;
+
+  @Autowired
+  private Receiver receiver;
+
+  @Test
+  public void testReceive() throws Exception {
+    sender.send("Hello Spring JMS ActiveMQ!");
+
+    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+  }
+}
